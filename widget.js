49c912ca3cca6e158e1621beb59ff71e267b7737
Wuddrum <s.apierens@gmail.com>
2019-03-21 15:28:57 +0200

    Initial commit

---
+++ widget.js
@@ -0,0 +1,18 @@
+!function(window, addEventListener, getElementById, widgetId) {
+    addEventListener('message', function(e) {
+        if (e.origin.split('://')[1] !== 'wuddrum.github.io') return;
+        getElementById(widgetId).style.height = e.data + 'px';
+    });
+    addEventListener('load', function() {
+        var widget = getElementById(widgetId);
+        var onWindowScroll = function() {
+            var widgetRect = widget.getBoundingClientRect();
+            if (widgetRect.right > 0 && widgetRect.left < window.innerWidth && widgetRect.bottom > 0 && widgetRect.top < window.innerHeight) {
+                window.removeEventListener('scroll', onWindowScroll);
+                widget.contentWindow.postMessage(86901, '*');
+            }
+        }
+        addEventListener('scroll', onWindowScroll);
+        onWindowScroll();
+    });
+}(window, window.addEventListener.bind(window), document.getElementById.bind(document), 'NewsWidget');
